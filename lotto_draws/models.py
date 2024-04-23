f09497fd28ca063ffbba8ae95a382ed2b8587a9e
Pumelela-Banca <pumelelabanca@gmail.com>
2024-04-23 21:09:23 +0200

    added lotto models for all games

--- lotto_draws/models.py
+++ lotto_draws/models.py
@@ -1,8 +1,57 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+
 
 
 class Daily(models.Model):
     """
     Dailly lotto model
     """
-    date = 
+    date = models.DateField()
+    numbers = JSONField(default=list)
+    bonus = models.IntegerField()
+
+
+class PowerBall(models.Model):
+    """
+    Powerball model
+    """
+    date = models.DateField()
+    numbers = JSONField(default=list)
+    powerball = models.IntegerField()
+
+
+class PowerBallPlus(models.Model):
+    """
+    Powerball Plus model
+    """
+    date = models.DateField()
+    numbers = JSONField(default=list)
+    powerball = models.IntegerField()
+
+
+class Lotto(models.Model):
+    """
+    Dailly lotto model
+    """
+    date = models.DateField()
+    numbers = JSONField(default=list)
+    bonus = models.IntegerField()
+
+
+class Lotto1(models.Model):
+    """
+    Dailly lotto model
+    """
+    date = models.DateField()
+    numbers = JSONField(default=list)
+    bonus = models.IntegerField()
+
+
+class Lotto2(models.Model):
+    """
+    Dailly lotto model
+    """
+    date = models.DateField()
+    numbers = JSONField(default=list)
+    bonus = models.IntegerField()
